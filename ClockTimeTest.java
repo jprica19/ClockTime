518237f525e3482497e43dfdbbd55a3d50f48b4e
Jordan <jordannprica@gmail.com>
2022-11-30 12:00:19 -0600

    I deleted a zero from seconds and minutes

---
+++ ClockTimeTest.java
@@ -0,0 +1,50 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class ClockTimeTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class ClockTimeTest
+{
+    /**
+     * Default constructor for test class ClockTimeTest
+     */
+    public ClockTimeTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testTime()
+    {
+        ClockTime clockTim1 = new ClockTime(11, 0, 0);
+        assertEquals("11:0:0", clockTim1.toString());
+    }
+}
+
